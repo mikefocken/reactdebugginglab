efa105185911d9e91fe6245e4449bddcb0d87163
mike focken <mikefocken@gmail.com>
2023-12-12 21:35:05 -0800

    completed questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import BookTable from './components/BookTable/BookTable';
+import "./App.css";
+import { useState, useEffect } from "react";
+import BookTable from "./components/BookTable/BookTable";
 // generateBookData is used to simulate source data.
 // debug the "books" state variable to identify what that data looks like.
 // **DO NOT USE generateBookData ANYWHERE ELSE IN THIS LAB!**
-import { generateBookData } from './data/bookData';
+import { generateBookData } from "./data/bookData";
 
 const App = () => {
-	const [books, setBooks] = useState();
+  const [books, setBooks] = useState([]);
 
-	useEffect(() => {
-		setBooks(generateBookData()); // **DO NOT EDIT THIS LINE!**
-	},[]);
-	console.log(books);
-	return (
-		<div>
-			<h1>Book Depot</h1>
-			<p>Title</p>
-		</div>
-	); 
+
+
+  useEffect(() => {
+    setBooks(generateBookData()); // **DO NOT EDIT THIS LINE!**
+  }, []);
+
+  return (
+    <div className="App">
+      <BookTable books={books} 
+     />
+     
+
+    </div>
+  );
 };
 
 export default App;
--- src/components/BookTable/BookTable.jsx
+++ src/components/BookTable/BookTable.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import './BookTable.css'
+import React from "react";
+import "./BookTable.css";
+import "./BookTable.jsx";
+import BookRow from "../BookRow/BookRow.jsx";
 // The BookList component is responsible for rendering the array of book objects
 // stored in the App component's state.
 // How can you give BookTable access to that books array?
 
-const BookTable = () => {
-	const bookItems = books.map((book) => <div>{book}</div>);
+const BookTable = ({ books }) => {
+  const bookItems = books.map((book) => <BookRow key={book.id} book={book} />);
 
-	return (
-		<div>
-			<h2>Book Table</h2>
-			<table className="table">
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Author</th>
-						<th>Date Published</th>
-						<th>Pages</th>
-					</tr>
-				</thead>
-				<tbody>{bookItems}</tbody>
-			</table>
-		</div>
-	);
+  return (
+    <div>
+      <h2>Book Table</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Date Published</th>
+            <th>Pages</th>
+          </tr>
+        </thead>
+        <tbody>{bookItems}</tbody>
+      </table>
+    </div>
+  );
 };
 
 export default BookTable;
